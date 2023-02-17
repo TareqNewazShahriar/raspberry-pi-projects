9ccee8c1a915dc0880d85e954016a045b6300d06
Tareq Newaz Shahriar <tareq.newaz.shahriar@gmail.com>
2023-02-17 19:20:48 +0600

    project cleared

--- home-iot/machine-app/README.md
+++
@@ -1,5 +0,0 @@
-# Home-IoT: Machine App
-
-## Required packages
-Python: pip3 (to install python3 packages), smbus2, PCF8591 (ADC driver code file)
-Node: onoff, firebase
--- misc/motion_sensor_test.py
+++
@@ -1,9 +0,0 @@
-from gpiozero import MotionSensor
-from time import sleep
-
-pir = MotionSensor(4)
-
-while True:
-  pir.wait_for_motion()
-  print('Movement detected')
-  pir.wait_for_no_motion()
